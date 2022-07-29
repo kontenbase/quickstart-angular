9545a7f446562b528968935115881d909fd86ef5
hasan <hsanromadon@gmail.com>
2022-07-29 15:39:41 +0700

    feat: edit profile page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { EditProfileComponent } from './edit-profile.component';
 
 const routes: Routes = [
+  {
+    path: 'profile',
+    component: EditProfileComponent,
+  },
+
   {
     path: '',
     component: HomeComponent,
